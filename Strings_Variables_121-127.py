0ff62875d7e971477fda05c654cdbb5609da1356
Dhruv Bajaj <Dhruv@Ambikas-MacBook-Pro.local>
2024-12-24 15:04:05 -0800

    “Commit”

--- Strings_Variables_121-127.py
+++ Strings_Variables_121-127.py
@@ -1,8 +1,39 @@
 print ("Video 121"+"\n")
 
 print("apple"+"banana")
-var1="apple"
+var1="apple "
 var2="banana"
 var3=var1+var2
 print("var1"+"var2")
-print("var1*3")
+print(var1*3)
+
+print ("Video 122"+"\n")
+
+print("/ is a escape character")
+
+print ("Video 123"+"\n")
+
+print(len(var1))
+print(len(var3))
+
+print ("Video 124"+"\n")
+
+var4="APPLE"
+print(var4.lower())
+var5="apple"
+print(var5.upper())
+print(var5.isupper())
+
+print ("Video 125"+"\n")
+
+print(var1[0])
+
+print ("Video 126"+"\n")
+
+print(var4.index("A"))
+print(var3.index("banana"))
+
+print ("Video 127"+"\n")
+
+print(var4.replace("A","Z"))
+print(var3.replace("banana","mango"))
