30bd5e2329c0097a0e191fc98b32de8d70490fb3
Dhruv Bajaj <Dhruv@Ambikas-MacBook-Pro.local>
2024-12-31 17:20:23 -0800

    delete the pri

---
+++ 156.py
@@ -0,0 +1,15 @@
+from math import sqrt
+import turtle
+pete= turtle.Turtle()
+a=int(input("What is the lenght of side a?: "))
+b=int(input("What is the lenght of side b?: "))
+print((a**2))
+print((b**2))
+c=sqrt((a**2)+(b**2))
+print("The value of c is :",c)
+pete.forward(a)
+pete.left(90)
+pete.forward(b)
+pete.left(135)
+pete.forward(c)
+turtle.done()
