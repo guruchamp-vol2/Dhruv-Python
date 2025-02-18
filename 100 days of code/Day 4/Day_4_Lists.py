5e138f2443218fbcda9db642b9a7fb3497832b09
Dhruv Bajaj <Dhruv@Ambikas-MacBook-Pro.local>
2025-02-17 20:25:15 -0800

    delete the pnt

---
+++ 100 days of code/Day 4/Day_4_Lists.py
@@ -0,0 +1,3 @@
+#This is a example of a list
+States_of_America=["Texas","Ohio"]
+#.append adds a item to a list
---
+++ 100 days of code/Day 4/Day_4_Pay_the_bill.py
@@ -0,0 +1,4 @@
+import random
+friends=["Alice","Bob","Charlie","David","Emanuel"]
+pernum=random.randint(0,4)
+print(friends[pernum])
